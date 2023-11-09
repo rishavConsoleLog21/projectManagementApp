fefb4012c80b97d7912d5e9a8aebfce46c1bec3f
rishavcool21 <kumarrishavcool21@gmail.com>
2023-11-09 15:58:59 +0530

    Added selected project & handle project selection in sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,22 @@ import React, { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProject from "./components/NoProject";
 import ProjectSidebar from "./components/ProjectSidebar";
+import SelectedProject from "./components/SelectedProject";
 
 function App() {
   const [project, setProject] = useState({
     selectedProjectId: undefined,
     projects: [],
   });
+
+  function handleSelectProject(projectId) {
+    setProject((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: projectId,
+      };
+    });
+  }
 
   function handleStartProject() {
     setProject((prevState) => {
@@ -42,17 +52,27 @@ function App() {
     });
   }
 
-  let content;
+  const selectedProject = project.projects.find(
+    (projectId) => projectId.id === project.selectedProjectId
+  );
+
+  let content = <SelectedProject project={selectedProject} />;
 
   if (project.selectedProjectId === null) {
-    content = <NewProject onAdd={handleAddProject} onCancel={handleCancelProject}/>;
+    content = (
+      <NewProject onAdd={handleAddProject} onCancel={handleCancelProject} />
+    );
   } else if (project.selectedProjectId === undefined) {
     content = <NoProject onStartAddProject={handleStartProject} />;
   }
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar onStartAddProject={handleStartProject} projects={project.projects}/>
+      <ProjectSidebar
+        onStartAddProject={handleStartProject}
+        projects={project.projects}
+        onSelectProject={handleSelectProject}
+      />
       {content}
     </main>
   );
--- src/components/ProjectSidebar.jsx
+++ src/components/ProjectSidebar.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import Button from "./Button";
 
-const ProjectSidebar = ({ onStartAddProject, projects }) => {
+
+const ProjectSidebar = ({
+  onStartAddProject,
+  projects,
+  onSelectProject,
+  selectedProjectId,
+}) => {
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-800 text-stone-50 md:w-72 rounded-r-xl">
       <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
@@ -11,13 +17,25 @@ const ProjectSidebar = ({ onStartAddProject, projects }) => {
         <Button onClick={onStartAddProject}>Start New Project</Button>
       </div>
       <ul className="mt-8">
-        {projects.map(project => (
-          <li key={project.id}>
-            <button className="w-full text-left px-2 py-1 rounded-sm my-1 text-slate-400 hover:text-slate-200 hover:bg-slate-800">
-              {project.title}
-            </button>
-          </li>
-        ))}
+        {projects.map((project) => {
+          let cssClasses =
+            "w-full text-left px-2 py-1 rounded-sm my-1 hover:text-slate-200 hover:bg-slate-800";
+          if (project.id === selectedProjectId) {
+            cssClasses += " bg-slate-800 text-slate-200";
+          } else {
+            cssClasses += " text-slate-400";
+          }
+          return (
+            <li key={project.id}>
+              <button
+                className={cssClasses}
+                onClick={() => onSelectProject(project.id)}
+              >
+                {project.title}
+              </button>
+            </li>
+          );
+        })}
       </ul>
     </aside>
   );
